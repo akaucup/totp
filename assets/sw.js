e807300e790bcde337190c4a34b93a1c123eaee7
akaucup <40891013+akaucup@users.noreply.github.com>
2025-06-24 20:58:25 +0700

    Add files via upload

---
+++ assets/sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "otp-cache-v1";
+const URLS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "https://cdn.jsdelivr.net/npm/otpauth/dist/otpauth.umd.min.js",
+];
+
+// Install event
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        // Optionally return a fallback page
+      })
+  );
+});
