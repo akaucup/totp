315d73b7bcaea8753f79366f91f0765e9d4fe5cb
akaucup <40891013+akaucup@users.noreply.github.com>
2025-06-25 05:52:04 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,41 +9,20 @@ const URLS_TO_CACHE = [
   "/assets/favicon.svg",
   "/assets/favicon.ico",
   "/assets/apple-touch-icon.png",
-  "https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap",
   "https://cdn.jsdelivr.net/npm/otpauth/dist/otpauth.umd.min.js"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(async cache => {
-      for (const url of URLS_TO_CACHE) {
+      await Promise.all(URLS_TO_CACHE.map(async url => {
         try {
-          const response = await fetch(url);
-          if (response.ok) {
-            await cache.put(url, response);
-          } else {
-            console.warn(`⚠️ Not OK response: ${url}`, response.status);
-          }
+          const response = await fetch(url, { mode: 'no-cors' });
+          await cache.put(url, response);
         } catch (err) {
           console.warn(`⚠️ Failed to cache: ${url}`, err);
         }
-      }
+      }));
     })
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
   );
 });
